8951a7704ba3183bb454188c9c2ab19942964375
crisrauber <cristianaugusto1@hotmail.com>
2021-03-17 22:20:44 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -12,7 +16,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -24,6 +28,28 @@ describe('Login Routes', () => {
           email: 'cristianaugusto1@hotmail.com',
           password: '123',
           passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('should return 200 on login', async () => {
+      const password = await hash('123', 12)
+
+      await accountCollection.insertOne({
+        name: 'cristian',
+        email: 'cristianaugusto1@hotmail.com',
+        password
+
+      })
+
+      await request(app).post('/api/login')
+        .send({
+
+          email: 'cristianaugusto1@hotmail.com',
+          password: '123'
+
         })
         .expect(200)
     })
