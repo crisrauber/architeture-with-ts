65a46baa5fd900bb1281625f65eb653c187e87ea
crisrauber <cristianaugusto1@hotmail.com>
2021-03-22 20:26:04 -0300

    chore: add engines with node version

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050,
-  jwtSecret: process.env.PORT || 'itjasASKDJMA5648===ASD4'
+  jwtSecret: process.env.JWT_TOKEN || 'itjasASKDJMA5648===ASD4'
 }
