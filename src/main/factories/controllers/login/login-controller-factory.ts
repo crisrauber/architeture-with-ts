82cfaacebfecc1a9c4c10de01bddc8c7750d3b1f
crisrauber <cristianaugusto1@hotmail.com>
2021-03-26 20:55:59 -0300

    refactor: movel login and signup controller to a folder

--- src/main/factories/controllers/login/login-controller-factory.ts
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -1,7 +1,7 @@
 import env from '../../../config/env'
 import { LogControllerDecorator } from '../../../decorators/log/log-controller-decorator'
 import { makeLoginValidation } from './login-validation-factory'
-import { LoginController } from '../../../../presentation/controllers/login/login-controller'
+import { LoginController } from '../../../../presentation/controllers/login/login/login-controller'
 import { Controller } from '../../../../presentation/protocols'
 import { LogMongoRepository } from '../../../../infra/db/mongodb/log/log-mongo-repository'
 import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
