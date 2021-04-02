3d9b2162047b463a095596ec3b0563e652d5fd6d
crisrauber <cristianaugusto1@hotmail.com>
2021-04-01 22:34:15 -0300

    test: ensure AddSurveyController return 204 on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,5 +1,5 @@
 import { httpRequest, Validation, AddSurvey, AddSurveyModel } from './add-survey-controller-protocols'
-import { badRequest, serverError } from '../../../helpers/http/http-helper'
+import { badRequest, serverError, noContent } from '../../../helpers/http/http-helper'
 import { AddSurveyController } from './add-survey-controller'
 
 const makeFakeRequest = (): httpRequest => ({
@@ -89,5 +89,13 @@ describe('AddSurvey Controller', () => {
     const httpResponse = await sut.handle(makeFakeRequest())
 
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('should return 204 on success', async () => {
+    const { sut } = makeSut()
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(noContent())
   })
 })
