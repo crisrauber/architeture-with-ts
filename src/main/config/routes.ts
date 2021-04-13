99765234e1a624ecc5a9fdd153c3ebb09efdc13e
crisrauber <cristianaugusto1@hotmail.com>
2021-04-12 22:23:58 -0300

    refactor: organize main layer folders

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -6,7 +6,7 @@ export default (app: Express): void => {
   app.use('/api', router)
   // eslint-disable-next-line node/no-path-concat
   readdirSync(`${__dirname}/../routes`).map(async file => {
-    if (!file.includes('.map')) {
+    if (!file.includes('.test.')) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
